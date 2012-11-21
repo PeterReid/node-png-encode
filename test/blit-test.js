2d5f948843e38bb5d3c1f0ff06b1d44884ec612e
Peter Reid <peter.d.reid@gmail.com>
2012-11-21 09:27:15 -0500

    Made an API to call nicely into the C functions
    There is a PNG object now that holds the buffer, width, and height so
    those don't have to be tracked separately.

--- test/blit-test.js
+++ test/blit-test.js
@@ -1,25 +1,15 @@
 var fs = require('fs');
 
-var png = require('../build/Release/node_png_encode')
+var Png = require('../lib/index')
 
-png.decode(fs.readFileSync('mixed-unlit.png'), function(err, towerWidth, towerHeight, towerBuffer) {
-  png.decode(fs.readFileSync('gradient.png'), function(err, bgWidth, bgHeight, bgBuffer) {
-    console.log(arguments);
-    console.log('going to blit!');
-    png.blitTransparently(bgBuffer, bgWidth, bgHeight, 
-             20,20,
-             towerBuffer, towerWidth, towerHeight,
-             0,0,
-             towerWidth, towerHeight);
-    png.blitTransparently(bgBuffer, bgWidth, bgHeight, 
-             -5,0,
-             towerBuffer, towerWidth, towerHeight,
-             0,0,
-             200, towerHeight);
-    console.log('encoding...');
-    png.encode(bgBuffer, bgWidth, bgHeight, function(err, buffer) {
-      console.log(arguments);
+Png.load(fs.readFileSync('mixed-unlit.png'), function(err, tower) {
+  if (err) return console.log(err);
+  
+  Png.load(fs.readFileSync('gradient.png'), function(err, background) {
+    tower.copy(background, 20, 20);
+    tower.copy(background, -5, 0);
+    background.encode(function(err, buffer) {
       fs.writeFileSync('./composedTransparently.png', buffer);
     });
-  })
+  });
 });
