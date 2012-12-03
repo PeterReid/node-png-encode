cb77964d19fee5a37d26d5ab9b4147de7399adf0
Peter Reid <peter.d.reid@gmail.com>
2012-12-03 14:39:29 -0600

    Added line drawing

--- src/blit.cc
+++ src/blit.cc
@@ -1,6 +1,7 @@
 #include <node.h>
 #include <node_buffer.h>
-
+#include <math.h>
+#include <algorithm>
 
 #include "macros.h"
 #include "blit.h"
@@ -19,32 +20,33 @@ static inline bool between_inex(int low_inclusive, int x, int high_exclusive) {
 static uint32_t blend(uint32_t bottom, uint32_t top) {
     // when destAlpha = 0, *destPixel = *sourcePixel;
     // when destAlpha=255, *destPixel
-    
+
     // it passes through the top layer first. With probability A, the color becomes R, G, B, 1.
     // THis contributes (sR*sA, sG*sA, sB*sA, sA)
-    
+
     // It passes through the bottom layer. With probability (1-sA)*dA, the color becomes dR, dG, dB, 1
     // THis contributes (x*dR, dG*x, dB*x, x) where x = (1-sA)*dA.
-    
+
     #define REDOF(x) ((x) & 0xff)
     #define GREENOF(x) (((x)>>8) & 0xff)
     #define BLUEOF(x) (((x)>>16) & 0xff)
     #define ALPHAOF(x) (((x)>>24) & 0xff)
     #define RGBA(r,g,b,a) ((a<<24) | (b<<16) | (g<<8) | (r))
-    
+
     // Otherwise, it is transparent. Probability 1 - sA - (1-sA)*dA
     int sR = REDOF(top), sG = GREENOF(top), sB = BLUEOF(top), sA = ALPHAOF(top);
     if (sA==0) return bottom;
-    
+    if (sA==255) return top;
+
     int dR = REDOF(bottom), dG = GREENOF(bottom), dB = BLUEOF(bottom), dA = ALPHAOF(bottom);
-    
+
     int dAAdjusted = ((255-sA)*dA)>>8;
-    
+
     int outA = sA + dAAdjusted;
     int outR = (sR*sA + dR*dAAdjusted) / outA;
     int outG = (sG*sA + dG*dAAdjusted) / outA;
     int outB = (sB*sA + dB*dAAdjusted) / outA;
-    
+
     return RGBA(outR, outG, outB, outA);
 }
 
@@ -61,7 +63,7 @@ Handle<Value> node_png_encode::BlitTransparently(const Arguments& args) {
     }
     REQUIRE_ARGUMENT_INTEGER(3, destX);
     REQUIRE_ARGUMENT_INTEGER(4, destY);
-    
+
     REQUIRE_ARGUMENT_BUFFER(5, sourceBuffer);
     REQUIRE_ARGUMENT_INTEGER(6, sourceBufferWidth);
     REQUIRE_ARGUMENT_INTEGER(7, sourceBufferHeight);
@@ -70,20 +72,20 @@ Handle<Value> node_png_encode::BlitTransparently(const Arguments& args) {
             String::New("Invalid source width or height"))
         );
     }
-    
-    
+
+
     REQUIRE_ARGUMENT_INTEGER(8, sourceX);
     REQUIRE_ARGUMENT_INTEGER(9, sourceY);
-    
+
     REQUIRE_ARGUMENT_INTEGER(10, copyWidth);
     REQUIRE_ARGUMENT_INTEGER(11, copyHeight);
-    
+
     uint32_t *destPixels = reinterpret_cast<uint32_t *>(Buffer::Data(destBuffer));
     uint32_t *sourcePixels = reinterpret_cast<uint32_t *>(Buffer::Data(sourceBuffer));
-    
+
     // TODO: Assumptions to avoid off-of-buffer!
-    
-    // Handle source starting in the transparent space above/left of the image 
+
+    // Handle source starting in the transparent space above/left of the image
     if (sourceX<0) {
       copyWidth += sourceX;
       destX -= sourceX;
@@ -94,7 +96,7 @@ Handle<Value> node_png_encode::BlitTransparently(const Arguments& args) {
       destY -= sourceY;
       sourceY = 0;
     }
-    
+
     // Handle copying to above and/or left of the dest
     if (destX<0) {
       copyWidth += destX;
@@ -106,20 +108,20 @@ Handle<Value> node_png_encode::BlitTransparently(const Arguments& args) {
       sourceY -= destY;
       destY = 0;
     }
-    
+
     // Handle going off the edge
     copyWidth = min(copyWidth, min(destBufferWidth-destX, sourceBufferWidth-sourceX));
     copyHeight = min(copyHeight, min(destBufferHeight-destY, sourceBufferHeight-sourceY));
-    
-    if (copyWidth>0 && copyHeight > 0 && 
-        between_inex(0,destX,destBufferWidth) && 
+
+    if (copyWidth>0 && copyHeight > 0 &&
+        between_inex(0,destX,destBufferWidth) &&
         between_inex(0,destY,destBufferHeight) &&
-        between_inex(0,sourceX,sourceBufferWidth) && 
+        between_inex(0,sourceX,sourceBufferWidth) &&
         between_inex(0,sourceY,sourceBufferHeight)
     ) {
       uint32_t *destRow = destPixels + destBufferWidth*destY + destX;
       uint32_t *sourceRow = sourcePixels + sourceBufferWidth*sourceY + sourceX;
-      
+
       for (int row = 0; row < copyHeight; row++, destRow += destBufferWidth, sourceRow += sourceBufferWidth) {
         uint32_t *destPixel = destRow;
         uint32_t *sourcePixel = sourceRow;
@@ -132,6 +134,105 @@ Handle<Value> node_png_encode::BlitTransparently(const Arguments& args) {
 }
 
 
+Handle<Value> node_png_encode::BlitTransparently(const Arguments& args) {
+    HandleScope scope;
+
+    REQUIRE_ARGUMENT_BUFFER(0, destBuffer);
+    REQUIRE_ARGUMENT_INTEGER(1, destBufferWidth);
+    REQUIRE_ARGUMENT_INTEGER(2, destBufferHeight);
+    if (destBufferWidth <= 0 || destBufferHeight <= 0 || destBufferWidth*destBufferHeight*BYTES_PER_PIXEL > (int)Buffer::Length(destBuffer)) {
+        return ThrowException(Exception::TypeError(
+            String::New("Invalid destination width or height"))
+        );
+    }
+    REQUIRE_ARGUMENT_INTEGER(3, destX);
+    REQUIRE_ARGUMENT_INTEGER(4, destY);
+
+    REQUIRE_ARGUMENT_BUFFER(5, sourceBuffer);
+    REQUIRE_ARGUMENT_INTEGER(6, sourceBufferWidth);
+    REQUIRE_ARGUMENT_INTEGER(7, sourceBufferHeight);
+    if (sourceBufferWidth <= 0 || sourceBufferHeight <= 0 || sourceBufferWidth*sourceBufferHeight*BYTES_PER_PIXEL > (int)Buffer::Length(sourceBuffer)) {
+        return ThrowException(Exception::TypeError(
+            String::New("Invalid source width or height"))
+        );
+    }
+
+
+    REQUIRE_ARGUMENT_INTEGER(8, sourceX);
+    REQUIRE_ARGUMENT_INTEGER(9, sourceY);
+
+    REQUIRE_ARGUMENT_INTEGER(10, copyWidth);
+    REQUIRE_ARGUMENT_INTEGER(11, copyHeight);
+
+    uint32_t *destPixels = reinterpret_cast<uint32_t *>(Buffer::Data(destBuffer));
+    uint32_t *sourcePixels = reinterpret_cast<uint32_t *>(Buffer::Data(sourceBuffer));
+
+    // TODO: Assumptions to avoid off-of-buffer!
+
+    // Handle source starting in the transparent space above/left of the image
+    if (sourceX<0) {
+      copyWidth += sourceX;
+      destX -= sourceX;
+      sourceX = 0;
+    }
+    if (sourceY<0) {
+      copyHeight += sourceY;
+      destY -= sourceY;
+      sourceY = 0;
+    }
+
+    // Handle copying to above and/or left of the dest
+    if (destX<0) {
+      copyWidth += destX;
+      sourceX -= destX;
+      destX = 0;
+    }
+    if (destY<0) {
+      copyHeight += destY;
+      sourceY -= destY;
+      destY = 0;
+    }
+
+    // Handle going off the edge
+    copyWidth = min(copyWidth, min(destBufferWidth-destX, sourceBufferWidth-sourceX));
+    copyHeight = min(copyHeight, min(destBufferHeight-destY, sourceBufferHeight-sourceY));
+
+    if (copyWidth>0 && copyHeight > 0 &&
+        between_inex(0,destX,destBufferWidth) &&
+        between_inex(0,destY,destBufferHeight) &&
+        between_inex(0,sourceX,sourceBufferWidth) &&
+        between_inex(0,sourceY,sourceBufferHeight)
+    ) {
+      uint32_t *destRow = destPixels + destBufferWidth*destY + destX;
+      uint32_t *sourceRow = sourcePixels + sourceBufferWidth*sourceY + sourceX;
+
+      for (int row = 0; row < copyHeight; row++, destRow += destBufferWidth, sourceRow += sourceBufferWidth) {
+        uint32_t *destPixel = destRow;
+        uint32_t *sourcePixel = sourceRow;
+        for (int col = 0; col < copyWidth; col++, destPixel++, sourcePixel++) {
+          *destPixel = blend(*destPixel, *sourcePixel);
+        }
+      }
+    }
+    return scope.Close(Null());
+}
+
+void plot(uint32_t *pixels, int w, int h, int x, int y, uint32_t color) {
+    //plot the pixel at (x, y) with brightness c (where 0 = c = 1)
+    if (x<0 || y<0 || x>=w || y>=h) return;
+
+    int alpha = ALPHAOF(color);
+    pixels[w*y + x] = //RGBA(alpha,alpha,alpha,255);//
+      blend(pixels[w*y + x], color);
+}
+
+int colorClamp(int x) {
+  if (x<0) return 0;
+  if (x>255) return 255;
+  return x;
+}
+
+//http://www.codeproject.com/Articles/13360/Antialiasing-Wu-Algorithm
 Handle<Value> node_png_encode::Line(const Arguments& args) {
     HandleScope scope;
 
@@ -143,12 +244,130 @@ Handle<Value> node_png_encode::Line(const Arguments& args) {
             String::New("Invalid destination width or height"))
         );
     }
-    REQUIRE_ARGUMENT_INTEGER(3, x0);
-    REQUIRE_ARGUMENT_INTEGER(4, y0);
-    REQUIRE_ARGUMENT_INTEGER(5, x1);
-    REQUIRE_ARGUMENT_INTEGER(6, y1);
-    
-    
-    
-    return scope.Close(Null());
+    uint32_t *destPixels = reinterpret_cast<uint32_t *>(Buffer::Data(destBuffer));
+
+    REQUIRE_ARGUMENT_INTEGER(3, X0);
+    REQUIRE_ARGUMENT_INTEGER(4, Y0);
+    REQUIRE_ARGUMENT_INTEGER(5, X1);
+    REQUIRE_ARGUMENT_INTEGER(6, Y1);
+
+    REQUIRE_ARGUMENT_INTEGER(7, red);
+    REQUIRE_ARGUMENT_INTEGER(8, green);
+    REQUIRE_ARGUMENT_INTEGER(9, blue);
+
+    const short NumLevels = 256;
+    const short IntensityBits = 8;
+
+    unsigned short IntensityShift, ErrorAdj, ErrorAcc;
+    unsigned short ErrorAccTemp, Weighting, WeightingComplementMask;
+    short DeltaX, DeltaY, Temp, XDir;
+    int BaseColor = RGBA(colorClamp(red), colorClamp(green), colorClamp(blue), 0);
+    const int fullAlpha = 0xff000000;
+    /* Make sure the line runs top to bottom */
+    if (Y0 > Y1) {
+        Temp = Y0; Y0 = Y1; Y1 = Temp;
+        Temp = X0; X0 = X1; X1 = Temp;
+    }
+   /* Draw the initial pixel, which is always exactly intersected by
+      the line and so needs no weighting */
+   plot(destPixels, destBufferWidth, destBufferHeight,X0, Y0, BaseColor|fullAlpha);
+
+   if ((DeltaX = X1 - X0) >= 0) {
+      XDir = 1;
+   } else {
+      XDir = -1;
+      DeltaX = -DeltaX; /* make DeltaX positive */
+   }
+   /* Special-case horizontal, vertical, and diagonal lines, which
+      require no weighting because they go right through the center of
+      every pixel */
+   if ((DeltaY = Y1 - Y0) == 0) {
+      /* Horizontal line */
+      while (DeltaX-- != 0) {
+         X0 += XDir;
+         plot(destPixels, destBufferWidth, destBufferHeight,X0, Y0, BaseColor|fullAlpha);
+      }
+      return scope.Close(Null());
+   }
+   if (DeltaX == 0) {
+      /* Vertical line */
+      do {
+         Y0++;
+         plot(destPixels, destBufferWidth, destBufferHeight,X0, Y0, BaseColor|fullAlpha);
+      } while (--DeltaY != 0);
+      return scope.Close(Null());
+   }
+   if (DeltaX == DeltaY) {
+      /* Diagonal line */
+      do {
+         X0 += XDir;
+         Y0++;
+         plot(destPixels, destBufferWidth, destBufferHeight,X0, Y0, BaseColor|fullAlpha);
+      } while (--DeltaY != 0);
+      return scope.Close(Null());
+   }
+   /* Line is not horizontal, diagonal, or vertical */
+   ErrorAcc = 0;  /* initialize the line error accumulator to 0 */
+   /* # of bits by which to shift ErrorAcc to get intensity level */
+   IntensityShift = 16 - IntensityBits;
+   /* Mask used to flip all bits in an intensity weighting, producing the
+      result (1 - intensity weighting) */
+   WeightingComplementMask = NumLevels - 1;
+   /* Is this an X-major or Y-major line? */
+   if (DeltaY > DeltaX) {
+      /* Y-major line; calculate 16-bit fixed-point fractional part of a
+         pixel that X advances each time Y advances 1 pixel, truncating the
+         result so that we won't overrun the endpoint along the X axis */
+      ErrorAdj = ((unsigned long) DeltaX << 16) / (unsigned long) DeltaY;
+      /* Draw all pixels other than the first and last */
+      while (--DeltaY) {
+         ErrorAccTemp = ErrorAcc;   /* remember currrent accumulated error */
+         ErrorAcc += ErrorAdj;      /* calculate error for next pixel */
+         if (ErrorAcc <= ErrorAccTemp) {
+            /* The error accumulator turned over, so advance the X coord */
+            X0 += XDir;
+         }
+         Y0++; /* Y-major, so always advance Y */
+         /* The IntensityBits most significant bits of ErrorAcc give us the
+            intensity weighting for this pixel, and the complement of the
+            weighting for the paired pixel */
+         Weighting = ErrorAcc >> IntensityShift;
+         plot(destPixels, destBufferWidth, destBufferHeight,X0, Y0, BaseColor | ((Weighting ^ WeightingComplementMask) << 24));
+         plot(destPixels, destBufferWidth, destBufferHeight,X0 + XDir, Y0,
+               BaseColor | (Weighting<<24));
+      }
+      /* Draw the final pixel, which is
+         always exactly intersected by the line
+         and so needs no weighting */
+      plot(destPixels, destBufferWidth, destBufferHeight,X1, Y1, BaseColor);
+      return scope.Close(Null());
+   }
+   /* It's an X-major line; calculate 16-bit fixed-point fractional part of a
+      pixel that Y advances each time X advances 1 pixel, truncating the
+      result to avoid overrunning the endpoint along the X axis */
+   ErrorAdj = ((unsigned long) DeltaY << 16) / (unsigned long) DeltaX;
+   /* Draw all pixels other than the first and last */
+   while (--DeltaX) {
+      ErrorAccTemp = ErrorAcc;   /* remember currrent accumulated error */
+      ErrorAcc += ErrorAdj;      /* calculate error for next pixel */
+      if (ErrorAcc <= ErrorAccTemp) {
+         /* The error accumulator turned over, so advance the Y coord */
+         Y0++;
+      }
+      X0 += XDir; /* X-major, so always advance X */
+      /* The IntensityBits most significant bits of ErrorAcc give us the
+         intensity weighting for this pixel, and the complement of the
+         weighting for the paired pixel */
+      Weighting = ErrorAcc >> IntensityShift;
+      plot(destPixels, destBufferWidth, destBufferHeight,X0, Y0, BaseColor | ((Weighting ^ WeightingComplementMask) << 24));
+      plot(destPixels, destBufferWidth, destBufferHeight,X0, Y0 + 1,
+            BaseColor | (Weighting<<24));
+   }
+   /* Draw the final pixel, which is always exactly intersected by the line
+      and so needs no weighting */
+   plot(destPixels, destBufferWidth, destBufferHeight,X1, Y1, BaseColor|fullAlpha);
+
+   return scope.Close(Null());
 }
+
+
--- src/main.cc
+++ src/main.cc
@@ -7,6 +7,7 @@ namespace {
     NODE_SET_METHOD(target, "encode", node_png_encode::Encode);
     NODE_SET_METHOD(target, "decode", node_png_encode::Decode);
     NODE_SET_METHOD(target, "blitTransparently", node_png_encode::BlitTransparently);
+    NODE_SET_METHOD(target, "line", node_png_encode::Line);
   }
 }
 
