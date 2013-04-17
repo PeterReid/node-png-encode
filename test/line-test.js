d767df68ba4ed3e8d49dfc360c5029c4757508c8
Peter Reid <peter.d.reid@gmail.com>
2013-04-17 11:33:47 -0400

    Draw dotted lines

--- test/line-test.js
+++ test/line-test.js
@@ -3,11 +3,12 @@ var fs = require('fs');
 var Png = require('../lib/index')
 
 var background = new Png(null, 200,200);
-
-for (var i = 0; i < 360; i+= 5) {
-  var angle = i / 180 * Math.PI;
-  background.line(100,100,Math.round(100 + 100*Math.cos(angle)), Math.round(100 + 100*Math.sin(angle)), 0,255,0);
-}
+background.buffer.fill(0xff);
+background.line(190,190, 210, 170, 0,0,0);
+//for (var i = 0; i < 360; i+= 5) {
+//  var angle = i / 180 * Math.PI;
+//  background.line(100,100,Math.round(100 + 100*Math.cos(angle)), Math.round(100 + 100*Math.sin(angle)), 0,255,0);
+//}
 background.encode(function(err, buffer) {
   fs.writeFileSync('./gradientWithLines.png', buffer);
 });
